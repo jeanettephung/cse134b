37458387918a30404501a8b5e9acf3758cb79ca2
Jeanette Phung <jeanettephung@Jeanettes-MacBook-Pro.local>
2017-07-26 14:05:15 -0700

    template complete, trying to put JSON in url

--- public/assets/js/dom_manip.js
+++ public/assets/js/dom_manip.js
@@ -1,26 +1,36 @@
-var mySoundBoard = {
-  
-  name : "Rick and Morty Cool Sounds",
-  sounds : [
-    
-    {
-      img : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/995085/cromulon.png',
-      sound : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/995085/goodjob.wav'
-    },
-    
-    {
-      img : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/995085/cromulon.png',
-      sound : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/995085/goodjob.wav'
-    } ,
-    
-        {
-      img : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/995085/cromulon.png',
-      sound : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/995085/goodjob.wav'
-    }  
-  ]
-  
-};
-
 window.onload = function () {
+  
+  var xmlhttp = new XMLHttpRequest();
+  var url = "./assets/json/got.json";
 
+  xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+      var myArr = JSON.parse(this.responseText);
+      myFunction(myArr);
+      }
+  };
+
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+  
+  
+  
+  var sounds = "";
+
+  var rows = document.getElementsByClassName('row');
+  var numPerRow = 0;
+  var curRow = 0;
+  
+  for (let i = 0; i < sounds.sounds.length; i++) {
+    var temp = document.getElementById('sound');
+    temp.content.querySelector('img').src= sounds.sounds[i].img;
+    temp.content.querySelector('img').alt="text";
+    var clone = document.importNode(temp.content, true);
+    rows[curRow].appendChild(clone);
+    numPerRow++;
+    if (numPerRow == 2) {
+      numPerRow = 0;
+      curRow++;
+    }
+  }
 }
