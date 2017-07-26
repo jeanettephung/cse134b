4fc451a38575f261fafacc6730a63912dc4620d5
Jeanette Phung <jeanettephung@Jeanettes-MacBook-Pro.local>
2017-07-26 13:50:48 -0700

    restart on dom manip

---
+++ public/assets/js/dom_manip.js
@@ -0,0 +1,26 @@
+var mySoundBoard = {
+  
+  name : "Rick and Morty Cool Sounds",
+  sounds : [
+    
+    {
+      img : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/995085/cromulon.png',
+      sound : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/995085/goodjob.wav'
+    },
+    
+    {
+      img : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/995085/cromulon.png',
+      sound : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/995085/goodjob.wav'
+    } ,
+    
+        {
+      img : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/995085/cromulon.png',
+      sound : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/995085/goodjob.wav'
+    }  
+  ]
+  
+};
+
+window.onload = function () {
+
+}
