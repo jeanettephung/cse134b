0f2040f2451c8d6bf82d686a514fa8028cb38b01
Jeanette Phung <jeanettephung@Jeanettes-MacBook-Pro.local>
2017-07-26 16:23:01 -0700

    sound event added to buttons

--- public/assets/js/dom_manip.js
+++ public/assets/js/dom_manip.js
@@ -1,7 +1,8 @@
 window.onload = function () {
+  
   var xmlhttp = new XMLHttpRequest();
-//  var url = "./assets/json/got.json";
-  var url = "https://gucci-b0d35.firebaseapp.com/assets/json/got.json";
+  var url = "./assets/json/got.json";
+//  var url = "https://gucci-b0d35.firebaseapp.com/assets/json/got.json";
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var data = JSON.parse(this.responseText);
@@ -11,8 +12,9 @@ window.onload = function () {
 
       for (let i = 0; i < data.soundboard.sounds.length; i++) {
         var temp = document.getElementById('sound');
-        temp.content.querySelector('img').src= data.soundboard.sounds[i].image;
-        temp.content.querySelector('img').alt="text";
+        temp.content.querySelector('img').src = data.soundboard.sounds[i].image;
+        temp.content.querySelector('img').alt = data.soundboard.sounds[i].alt;
+        temp.content.querySelector('source').src = data.soundboard.sounds[i].sound;
         var clone = document.importNode(temp.content, true);
         rows[curRow].appendChild(clone);
         numPerRow++;
@@ -20,9 +22,27 @@ window.onload = function () {
           numPerRow = 0;
           curRow++;
         }
+        var soundList = document.getElementsByClassName('sound');
+        var current = soundList[soundList.length-1];
+        var btns = current.getElementsByClassName('btn');
+        for (let j = 0; j < btns.length; j++) {
+          btns[j].addEventListener('click', function () {
+            var spanClass = this.querySelector('span').className;
+            var parentTag = this.parentElement.parentElement.getElementsByTagName('audio')[0];
+            if (spanClass.startsWith("glyphicon glyphicon-play")) {
+              parentTag.play();
+            } else if (spanClass.startsWith("glyphicon glyphicon-pause")) {
+              parentTag.pause();
+            } else {
+              parentTag.pause();
+              parentTag.currentTime = 0;      
+            };
+          });
+        }
       }
     }
   };
   xmlhttp.open("GET", url, true);
   xmlhttp.send();
+  
 }
