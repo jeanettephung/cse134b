0b7629d8639a5fa75b5c210d79cc1a11f812a336
Jeanette Phung <jeanettephung@Jeanettes-MacBook-Pro.local>
2017-08-01 11:59:38 -0700

    worked on firebase, adding rest of assets to cache

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -5,7 +5,14 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('v1')
       .then(cache => cache.addAll([
-        '/assets/img/1_cersei.jpg'
+         '/soundboard-hw4-vanilla.html',
+         '/assets/css/dom.css',
+         '/assets/js/hw4_javascript.js',
+         '/assets/json/got.json',
+         '/assets/json/ram.json',
+         '/assets/img/gameofthrones/*.jpg',
+         '/assets/img/rickandmorty/*.jpg',
+         '/assets/audio/*.wav'
       ]))
   );
   console.log(caches);
@@ -14,18 +21,18 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   console.log("fetching");
-//  const url = new URL(event.request.url);
-//  console.log("URL: "+url);
+  const url = new URL(event.request.url);
+  console.log("URL: "+url);
   
-//  if (url.origin == location.origin && url.pathname == '/') {
-//    console.log("condition met")
-//    event.respondWith(caches.match('/soundboard-hw4-vanilla.html'));
-//    return;
-//  }
+  if (url.origin == location.origin && url.pathname == '/') {
+    console.log("condition met")
+    event.respondWith(caches.match('/soundboard-hw4-vanilla.html'));
+    return;
+  }
   
-//  console.log("condition not met");
-//  event.respondWith (
-//    caches.match(event.request)
-//      .then (response => response || fetch(event.request))
-//  );
+  console.log("condition not met");
+  event.respondWith (
+    caches.match(event.request)
+      .then (response => response || fetch(event.request))
+  );
 });
